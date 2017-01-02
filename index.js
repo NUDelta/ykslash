a1c4ae9c40a84ef8e876206abe15fe2a690b7f43
sashaweiss <sasha.weiss@u.northwestern.edu>
2017-01-02 16:42:06 -0600

    Test slash command

---
+++ index.js
@@ -0,0 +1,43 @@
+var app = require('express')();
+var bodyParser = require('body-parser');
+
+var VERIFY_TOKEN = process.env.SLACK_VERIFY_TOKEN
+if (!VERIFY_TOKEN) {
+  console.error('SLACK_VERIFY_TOKEN is required')
+  process.exit(1)
+}
+
+// Use Beep Boop provided PORT - 8000 for dev
+var PORT = process.env.PORT || 8000
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/sprint', function(req, res) {
+  if (req.body.token != VERIFY_TOKEN) {
+    return res.status(401).send('Unauthorized');
+  }
+
+  var message = 'hey!';
+
+  if (req.body.text == 'help') {
+    message = 'Helping!';
+  }
+
+  res.status(200).send({
+    'response_type': 'in_channel',
+    'text': message,
+  });
+});
+
+app.get('/sprint', function(req, res) {
+  res.status(200).send('Ok');
+});
+
+app.listen(PORT, function(err) {
+  if (err) {
+    return console.error('Error spinning up', err);
+  }
+
+  console.log('Server started');
+});
